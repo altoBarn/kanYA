ba80953f3c386f5b1646a54ee8f98f4e93748400
altoBarn <alto123@zoho.com>
2019-10-21 12:24:31 +0700

    Update iBOT.php

--- iBOT.php
+++ iBOT.php
@@ -6,7 +6,7 @@ $content = file_get_contents('php://input');
 
 //kkk add
 //$url = 'http://202.47.242.182:8088/api/line';
-$url = 'http://202.47.249.98:8088/api/line';
+$url = 'http://202.47.249.98:45457/api/line';
 
 //$url = 'http://202.47.249.98:8089/api/todo';
 

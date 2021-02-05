31edaf83fe34b3184c48752e927001300702db0c
altoBarn <alto123@zoho.com>
2021-02-05 17:29:49 +0700

    Update iBOT.php

--- iBOT.php
+++ iBOT.php
@@ -7,7 +7,7 @@ $content = file_get_contents('php://input');
 //kkk add
 //$url = 'http://202.47.242.182:8088/api/line';
 //$url = 'http://202.47.249.98:45455/api/line';
-$url = 'http://202.47.226.114:45455/api/line';
+$url = 'http://202.47.226.124:45455/api/line';
 
 
 $headers = array('Content-Type: application/json');
